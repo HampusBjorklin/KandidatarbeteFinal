2e3a537eb68192bb0d5e2f2f38c635c938741532
Oscar Widenfalk <oscar@widenfalk.se>
2021-04-19 10:26:42 +0200

    UTF encoding issue for windows fixed in counterargument_db.py. Moved main.py to a main function. Changed nltk_test.py to the correct trolley path

--- main.py
+++ main.py
@@ -7,29 +7,35 @@ from counterargument_db import create_dataframe
 from bert_encoding import bert_encoding
 from bot_response import counter_argument
 
-# Check if database with claims and counterarguments and bert embeddings already been created, otherwise create.
-if os.path.isfile('embeddings_df.pkl'):
-    dataframe = pd.read_pickle('embeddings_df.pkl')
-else:
-    if os.path.isfile('Pickles/Trolley.pkl'):
-        dataframe = pd.read_pickle('Pickles/trolley.pkl')
+
+def main():
+    # Check if database with claims and counterarguments and bert embeddings already been created, otherwise create.
+    if os.path.isfile('embeddings_df.pkl'):
+        dataframe = pd.read_pickle('embeddings_df.pkl')
     else:
-        create_dataframe()
-        dataframe = pd.read_pickle('Pickles/trolley.pkl')
+        if os.path.isfile('Pickles/Trolley.pkl'):
+            dataframe = pd.read_pickle('Pickles/trolley.pkl')
+        else:
+            create_dataframe()
+            dataframe = pd.read_pickle('Pickles/trolley.pkl')
 
-    bert_encoding(dataframe)
-    embeddings = pd.read_pickle('embeddings_df.pkl')
+        bert_encoding(dataframe)
+        embeddings = pd.read_pickle('embeddings_df.pkl')
 
-# Start bot conversation...
-print('BOT: As a bot I am a terrible debater and always agree')
+    # Start bot conversation...
+    print('BOT: As a bot I am a terrible debater and always agree')
 
-exit_words = ['bye', 'fuckoff', 'quit', 'exit', 'cya', 'goodbye']
+    exit_words = ['bye', 'fuckoff', 'quit', 'exit', 'cya', 'goodbye']
 
-while(True):
-    user_input = input()
-    user_input = user_input.lower()
-    if user_input in exit_words:
-        print('BOT: Good talk')
-        break
-    else:
-        print('BOT: '+ counter_argument(user_input, dataframe))
+    while(True):
+        user_input = input()
+        user_input = user_input.lower()
+        if user_input in exit_words:
+            print('BOT: Good talk')
+            break
+        else:
+            print('BOT: '+ counter_argument(user_input, dataframe))
+
+
+if __name__ == '__main__':
+    main()
